d4e7e17c17cff2472643a2fe110727a4283ed3d5
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2019-10-09 15:18:03 -0400

    Update search_text_file.py

--- search_text_file.py
+++ search_text_file.py
@@ -1,11 +1,11 @@
-words = ['Bootstrap (Twitter)', 'Apache Commons Codec', 'Commons IO', 'Apache Commons Lang', 'Apache Commons Logging', 'Apache HttpComponents Client', 'SLF4J API Module', 'Guava: Google Core Libraries for Java', 'Spring Framework', 'jackson-databind', 'JUnit', 'Visionmedia Debug', 'jackson-annotations', 'Apache Commons Collections', 'jQuery UI', 'ASM', 'BabelJs', 'node-uuid', 'FindBugs jsr305', 'Javassist', 'Commander.js', 'JavaBeans Activation Framework', 'Apache Commons Compress', 'UglifyJS', 'AOP Alliance', 'LessCss', 'yargs', 'Apache Xalan (Java)', 'range-parser', 'Apache Ant']
+words = [ 'word1', 'word2', 'foo']
 
-daSearch = open("NVD_Dashboard-Mapping.html", "r").read()
+daSearch = open("foo.txt", "r").read()
 
 for word in words:
     if word in daSearch:
        print(word)
-#    else:
-#       print("not found")
+    else:
+       print("not found")
 
 print("\nDone")
